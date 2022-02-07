bcf77d0310a29675cdb68a0a1c22d50a81ca092b
Surasit Khokkruad <sn0wyfoxs.developer@gmail.com>
2022-02-07 18:59:12 +0700

    feat: add icon to header and add links to footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import Image from "next/image";
 import axios from "axios";
 import { Ticker } from "../interfaces/Ticker";
+import Link from "next/link";
 
 type Data = {
   THB_KUB: number;
@@ -108,9 +109,17 @@ const Home: NextPage<Data> = ({ THB_KUB, THB_USDT }) => {
         <title>MMV EZ - Home</title>
       </Head>
 
-      <nav className="navbar sticky top-0 flex space-x-2 bg-white shadow-lg">
-        <div className="flex-1">
-          <span className="text-lg font-bold select-none">MMV EZ</span>
+      <nav className="navbar sticky top-0 z-50 flex space-x-2 bg-white shadow-lg">
+        <div className="flex flex-1 ml-2 space-x-2">
+          <div className="w-10 h-10">
+            <Image
+              src="/icons/chicken_loading.gif"
+              alt="chicken_loading"
+              width={512}
+              height={512}
+            />
+          </div>
+          <span className="font-bold select-none">MMV EZ</span>
         </div>
 
         {/* <div className="lg:flex flex-none hidden">
@@ -187,7 +196,7 @@ const Home: NextPage<Data> = ({ THB_KUB, THB_USDT }) => {
         </div>
       </nav>
 
-      <div className="sm:max-w-screen-sm container self-center flex-1 p-4 space-y-4">
+      <div className="sm:max-w-screen-sm container z-0 self-center flex-1 p-4 space-y-4">
         <div className="grid grid-cols-2 gap-4">
           <div className="card flex flex-row space-x-2 overflow-hidden bg-white shadow-lg">
             <div className="bg-zinc-700 flex flex-col items-center justify-center w-12 h-12 p-2">
@@ -574,9 +583,50 @@ const Home: NextPage<Data> = ({ THB_KUB, THB_USDT }) => {
         </div>
       </div>
 
-      <footer className="footer bg-base-300 text-base-content footer-center p-4">
-        <div>
-          <p>Copyright © 2022 - All right reserved by Artzeeker</p>
+      <footer className="footer bg-neutral text-neutral-content items-center gap-4 p-4">
+        <div className="items-center grid-flow-col">
+          <p>Copyright © 2022 - All right reserved</p>
+        </div>
+        <div className="md:place-self-center md:justify-self-end grid-flow-col gap-4">
+          <Link href="https://www.facebook.com/artzeeker/">
+            <a target="_blank">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                className="fill-current"
+              >
+                <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
+              </svg>
+            </a>
+          </Link>
+          <Link href="https://www.youtube.com/channel/UCyqsJtVoVGO98oHvaI14UXg">
+            <a target="_blank">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                className="fill-current"
+              >
+                <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
+              </svg>
+            </a>
+          </Link>
+          <Link href="https://twitter.com/artzeeker">
+            <a target="_blank">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                className="fill-current"
+              >
+                <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
+              </svg>
+            </a>
+          </Link>
         </div>
       </footer>
     </div>
