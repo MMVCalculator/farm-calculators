150fcb990c5c4b2a3bec708552af587876eaa246
Surasit Khokkruad <sn0wyfoxs.developer@gmail.com>
2022-03-18 22:30:40 +0700

    fix: split home page codes into components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import type { GetServerSideProps, NextPage } from "next";
 import Head from "next/head";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import Image from "next/image";
 import axios from "axios";
 import {
@@ -10,6 +10,7 @@ import {
 } from "../interfaces/responses";
 import Link from "next/link";
 import useSWR from "swr";
+import Navbar from "../components/Navbar";
 
 type Props = {
   THB_KUB: number;
@@ -92,8 +93,6 @@ const Home: NextPage<Props> = ({ THB_KUB, THB_USDT, latestRates, usdLumi }) => {
   // );
 
   useEffect(() => {
-    document.getElementsByTagName("html")[0].dataset.theme = "light";
-
     const BASE_API_URL_BITKUB = "wss://api.bitkub.com/websocket-api";
 
     const wsBitkub = new WebSocket(
@@ -217,129 +216,44 @@ const Home: NextPage<Props> = ({ THB_KUB, THB_USDT, latestRates, usdLumi }) => {
   ]);
 
   return (
-    <div className="bg-slate-100 flex flex-col w-screen h-screen overflow-auto">
+    <div className="bg-base-200 flex flex-col w-screen h-screen overflow-auto min-w-[20rem]">
       <Head>
         <title>MMVez</title>
       </Head>
 
-      <nav className="navbar sticky top-0 z-50 flex space-x-2 bg-white shadow-lg">
-        <div className="flex flex-1 ml-2 space-x-2">
-          <div className="aspect-square flex flex-col items-center justify-center w-10 h-10">
-            <Image
-              src="/icons/mmv_logo.png"
-              alt="mmv-logo"
-              width={960}
-              height={960}
-            />
-          </div>
-          <span className="select-none font-[Chewy]">MMVez</span>
-        </div>
-
-        {/* <div className="lg:flex flex-none hidden">
-          <ul className="flex items-stretch space-x-2">
-            <a className="btn btn-ghost btn-sm rounded-btn">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                className="inline-block w-5 mr-2 stroke-current"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-                ></path>
-              </svg>
-              Likes
-            </a>
-
-            <a className="btn btn-ghost btn-sm rounded-btn">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                className="inline-block w-5 mr-2 stroke-current"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-                ></path>
-              </svg>
-              Notifications
-            </a>
-
-            <a className="btn btn-ghost btn-sm rounded-btn">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                className="inline-block w-5 mr-2 stroke-current"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"
-                ></path>
-              </svg>
-              Files
-            </a>
-          </ul>
-        </div> */}
-
-        <div className="flex-none">
-          <button className="btn btn-square btn-ghost">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              className="inline-block w-6 h-6 stroke-current"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h16M4 18h16"
-              ></path>
-            </svg>
-          </button>
-        </div>
-      </nav>
+      <Navbar />
 
       <div className="sm:max-w-screen-sm container z-0 self-center flex-1 p-4 space-y-4">
         <div className="grid grid-cols-2 gap-4">
-          <div className="card flex flex-row space-x-2 overflow-hidden bg-white shadow-lg">
-            <div className="bg-zinc-700 flex flex-col items-center justify-center w-12 h-12 p-2">
+          <div className="card bg-base-100 flex flex-row space-x-2 overflow-hidden shadow-lg">
+            <div className="bg-neutral flex flex-col items-center justify-center w-12 h-12 p-2">
               <Image src="/icons/kub.png" alt="kub" width={80} height={80} />
             </div>
             <div className="flex flex-col items-center justify-center flex-1 text-center">
-              <h1 className="text-zinc-700 font-bold">{thbKub}</h1>
-              <p className="text-2xs text-stone-400 font-medium">THB/KUB</p>
+              <h1 className="font-bold">{thbKub}</h1>
+              <p className="text-2xs">THB/KUB</p>
             </div>
           </div>
-          <div className="card flex flex-row space-x-2 overflow-hidden bg-white shadow-lg">
-            <div className="bg-zinc-700 flex flex-col items-center justify-center w-12 h-12 p-2">
+          <div className="card bg-base-100 flex flex-row space-x-2 overflow-hidden shadow-lg">
+            <div className="bg-neutral flex flex-col items-center justify-center w-12 h-12 p-2">
               <Image src="/icons/usdt.png" alt="usdt" width={80} height={80} />
             </div>
             <div className="flex flex-col items-center justify-center flex-1 text-center">
-              <h1 className="text-zinc-700 font-bold">{thbUsdt}</h1>
-              <p className="text-2xs text-stone-400 font-medium">THB/USDT</p>
+              <h1 className="font-bold">{thbUsdt}</h1>
+              <p className="text-2xs">THB/USDT</p>
             </div>
           </div>
-          <div className="card flex flex-row space-x-2 overflow-visible bg-white shadow-lg">
-            <div className="bg-zinc-700 rounded-l-2xl flex flex-col items-center justify-center w-12 h-12 p-2">
+          <div className="card bg-base-100 flex flex-row space-x-2 overflow-hidden shadow-lg">
+            <div className="bg-neutral flex flex-col items-center justify-center w-12 h-12 p-2">
               <Image src="/icons/lumi.png" alt="lumi" width={80} height={80} />
             </div>
             <div className="relative flex flex-col items-center justify-center flex-1 text-center">
-              <h1 className="text-zinc-700 font-bold">{thbLumi.toFixed(2)}</h1>
-              <p className="text-2xs text-stone-400 font-medium">THB/LUMI</p>
+              <h1 className="font-bold">{parseFloat(thbLumi.toFixed(2))}</h1>
+              <p className="text-2xs">THB/LUMI</p>
             </div>
           </div>
-          <div className="card flex flex-row space-x-2 overflow-visible bg-white shadow-lg">
-            <div className="bg-zinc-700 rounded-l-2xl flex flex-col items-center justify-center w-12 h-12 p-2">
+          <div className="card bg-base-100 flex flex-row space-x-2 overflow-hidden shadow-lg">
+            <div className="bg-neutral flex flex-col items-center justify-center w-12 h-12 p-2">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="w-12 h-12 -m-1"
@@ -355,13 +269,13 @@ const Home: NextPage<Props> = ({ THB_KUB, THB_USDT, latestRates, usdLumi }) => {
               </svg>
             </div>
             <div className="relative flex flex-col items-center justify-center flex-1 text-center">
-              <h1 className="text-zinc-700 font-bold">{thbUsd.toFixed(2)}</h1>
-              <p className="text-2xs text-stone-400 font-medium">THB/USD</p>
+              <h1 className="font-bold">{parseFloat(thbUsd.toFixed(2))}</h1>
+              <p className="text-2xs">THB/USD</p>
             </div>
           </div>
         </div>
 
-        <div className="card flex flex-col p-4 space-y-4 overflow-hidden bg-white shadow-lg">
+        <div className="card bg-base-100 flex flex-col p-4 space-y-4 overflow-hidden shadow-lg">
           <h1 className="text-lg font-medium text-center">คำนวณผลผลิตต่อวัน</h1>
 
           <div className="btn-group self-center">
@@ -371,7 +285,6 @@ const Home: NextPage<Props> = ({ THB_KUB, THB_USDT, latestRates, usdLumi }) => {
               }`}
               onClick={() => {
                 setPlantKind("SEED");
-
                 switch (seedKind) {
                   case "TOMATO":
                     setRewardMultiplier(8);
@@ -396,7 +309,6 @@ const Home: NextPage<Props> = ({ THB_KUB, THB_USDT, latestRates, usdLumi }) => {
               }`}
               onClick={() => {
                 setPlantKind("STEM");
-
                 switch (stemLP) {
                   case "LKKUB":
                     switch (seedKind) {
@@ -445,7 +357,6 @@ const Home: NextPage<Props> = ({ THB_KUB, THB_USDT, latestRates, usdLumi }) => {
                 }`}
                 onClick={() => {
                   setStemLP("LKKUB");
-
                   switch (seedKind) {
                     case "TOMATO":
                       setRewardMultiplier(24);
@@ -470,7 +381,6 @@ const Home: NextPage<Props> = ({ THB_KUB, THB_USDT, latestRates, usdLumi }) => {
                 }`}
                 onClick={() => {
                   setStemLP("LKUSDT");
-
                   switch (seedKind) {
                     case "TOMATO":
                       setRewardMultiplier(20);
@@ -508,7 +418,6 @@ const Home: NextPage<Props> = ({ THB_KUB, THB_USDT, latestRates, usdLumi }) => {
               }`}
               onClick={() => {
                 setSeedKind("TOMATO");
-
                 switch (plantKind) {
                   case "SEED":
                     setRewardMultiplier(8);
@@ -534,7 +443,6 @@ const Home: NextPage<Props> = ({ THB_KUB, THB_USDT, latestRates, usdLumi }) => {
               }`}
               onClick={() => {
                 setSeedKind("CORN");
-
                 switch (plantKind) {
                   case "SEED":
                     setRewardMultiplier(8);
@@ -560,7 +468,6 @@ const Home: NextPage<Props> = ({ THB_KUB, THB_USDT, latestRates, usdLumi }) => {
               }`}
               onClick={() => {
                 setSeedKind("CABBAGE");
-
                 switch (plantKind) {
                   case "SEED":
                     setRewardMultiplier(8);
@@ -586,7 +493,6 @@ const Home: NextPage<Props> = ({ THB_KUB, THB_USDT, latestRates, usdLumi }) => {
               }`}
               onClick={() => {
                 setSeedKind("CARROT");
-
                 switch (plantKind) {
                   case "SEED":
                     setRewardMultiplier(12);
@@ -841,10 +747,8 @@ const Home: NextPage<Props> = ({ THB_KUB, THB_USDT, latestRates, usdLumi }) => {
     </div>
   );
 };
-
 export const getServerSideProps: GetServerSideProps<Props> = async () => {
   const now = Math.floor(Date.now() / 1000);
-
   const responses = await Promise.all([
     axios.get("https://api.bitkub.com/api/market/ticker?sym=THB_KUB"),
     axios.get("https://api.bitkub.com/api/market/ticker?sym=THB_USDT"),
@@ -857,15 +761,14 @@ export const getServerSideProps: GetServerSideProps<Props> = async () => {
       }&to=${now}&currencyCode=USD`
     ),
   ]);
-
   return {
     props: {
       THB_KUB: responses[0].data.THB_KUB.last,
+
       THB_USDT: responses[1].data.THB_USDT.last,
       latestRates: responses[2].data,
       usdLumi: responses[3].data.c[responses[3].data.c.length - 1],
     },
   };
 };
-
 export default Home;
