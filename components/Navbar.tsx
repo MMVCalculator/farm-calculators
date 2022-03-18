95f88ad2cd447e37325c85c7dbea72220c612385
Surasit Khokkruad <sn0wyfoxs.developer@gmail.com>
2022-03-19 01:22:26 +0700

    feat: add navbar component

---
+++ components/Navbar.tsx
@@ -0,0 +1,99 @@
+import React, { useEffect } from "react";
+import Image from "next/image";
+import useLocalStorageState from "use-local-storage-state";
+import Themes from "./Themes";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar bg-base-100 sticky top-0 z-50 flex space-x-2 shadow-lg">
+      <div className="flex flex-1 ml-2 space-x-2">
+        <div className="aspect-square flex flex-col items-center justify-center w-10 h-10">
+          <Image
+            src="/icons/mmv_logo.png"
+            alt="mmv-logo"
+            width={40}
+            height={40}
+          />
+        </div>
+        <span className="select-none font-[Chewy]">MMVez</span>
+      </div>
+
+      {/* <div className="lg:flex flex-none hidden">
+        <ul className="flex items-stretch space-x-2">
+          <a className="btn btn-ghost btn-sm rounded-btn">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              className="inline-block w-5 mr-2 stroke-current"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+              ></path>
+            </svg>
+            Likes
+          </a>
+
+          <a className="btn btn-ghost btn-sm rounded-btn">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              className="inline-block w-5 mr-2 stroke-current"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+              ></path>
+            </svg>
+            Notifications
+          </a>
+
+          <a className="btn btn-ghost btn-sm rounded-btn">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              className="inline-block w-5 mr-2 stroke-current"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"
+              ></path>
+            </svg>
+            Files
+          </a>
+        </ul>
+      </div> */}
+
+      <Themes />
+
+      <div className="flex-none">
+        <button className="btn btn-square btn-ghost">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            className="inline-block w-6 h-6 stroke-current"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4 6h16M4 12h16M4 18h16"
+            ></path>
+          </svg>
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
