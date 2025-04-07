35fea5142177a406fd555fb018ea97b1748fcd84
MMVCalculator <pisanupong001@gmail.com>
2025-04-07 22:21:18 +0700

    edit

--- pages/index.tsx
+++ pages/index.tsx
@@ -37,7 +37,7 @@ const Home: NextPage = () => {
   }>();
   const [stemLkkubPrice, setStemLkkubPrice] = useState<number>();
   const [seedKind, setSeedKind] = useState<SeedKind>("TOMATO");
-  const [rewardMultiplier, setRewardMultiplier] = useState<RewardMultiplier>(5);
+  const [rewardMultiplier, setRewardMultiplier] = useState<RewardMultiplier>(1);
   const [plantAmount, setPlantAmount] = useState<number | null>(null);
   const [totalLiquidity, setTotalLiquidity] = useState<number | null>(null);
   const [totalLiquidities, setTotalLiquidities] = useState<
