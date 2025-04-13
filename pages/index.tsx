5727b6d4a271297a5a25e1ff9a257ca8f3c176f8
MMVCalculator <pisanupong001@gmail.com>
2025-04-13 15:18:16 +0700

    Update index
    Add FISH FOOD

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,7 @@ import { CSSTransition } from "react-transition-group";
 
 type PlantKind = "SEED" | "STEM" | "LUMI";
 type StemLP = "LKKUB" | "LKUSDT";
-type SeedKind = "TOMATO" | "CORN" | "CABBAGE" | "CARROT" | "COFFEE" | "BLUEBERRY";
+type SeedKind = "TOMATO" | "CORN" | "CABBAGE" | "CARROT" | "COFFEE" | "BLUEBERRY" | "FISH FOOD";
 type RewardMultiplier = 1 | 2 | 3 | 4 | 6 | 8 | 14 | 18;
 
 const Home: NextPage = () => {
@@ -231,6 +231,10 @@ const Home: NextPage = () => {
           name: "seedFarmBlueberry",
           address: "018391166b47632b95fd50b3c37e8b25cc61ea29",
         },
+        {
+          name: "seedFarmFishfood",
+          address: "0x2CA957C560151148aA4AB677f4171E265e76806E",
+        },
       ];
 
       const totalLiquiditiesResponse = await Promise.all(
@@ -368,6 +372,8 @@ const Home: NextPage = () => {
             ? totalLiquidities[5]?.totalLiquidity
             : seedKind === "BLUEBERRY"
             ? totalLiquidities[6]?.totalLiquidity
+            : seedKind === "FISH FOOD"
+            ? totalLiquidities[7]?.totalLiquidity
             : Infinity) +
             (plantAmount || 0));
 
@@ -671,6 +677,9 @@ const Home: NextPage = () => {
                   case "BLUEBERRY":
                     setRewardMultiplier(2);
                     break;
+                  case "FISH FOOD":
+                    setRewardMultiplier(8);
+                    break;
                 }
               }}
             >
@@ -945,6 +954,23 @@ const Home: NextPage = () => {
               >
                 BLUEBERRY
               </button>
+
+              <button
+                className={`btn btn-xs${
+                  seedKind === "FISH FOOD" ? " btn-active" : ""
+                }`}
+                onClick={() => {
+                  setSeedKind("FISH FOOD");
+                  switch (plantKind) {
+                    case "SEED":
+                      setRewardMultiplier(8);
+                      break;
+                  
+                  }
+                }}
+              >
+                FISH FOOD
+              </button>
             </div>
           )}
 
@@ -1094,6 +1120,12 @@ const Home: NextPage = () => {
                       ? (totalLiquidities[6] &&
                           parseFloat(
                             totalLiquidities[6].totalLiquidity.toFixed(2)
+                          ).toLocaleString("th-TH")) ||
+                        "-"
+                   : seedKind === "FISH FOOD"
+                      ? (totalLiquidities[7] &&
+                          parseFloat(
+                            totalLiquidities[7].totalLiquidity.toFixed(2)
                           ).toLocaleString("th-TH")) ||
                         "-"
                       : "-"
